d8a171583f34b68421ba95fd304f4a3873769af2
Mezaque <mezaquegit@gmail.com>
2024-11-06 21:22:57 -0300

    dm1q: Welcome voltage project
    Change-Id: Ifa8132fb273f6791ce68eacb5cad2c9736fc369b

--- voltage_dm1q.mk
+++ voltage_dm1q.mk
@@ -12,9 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/dm1q/device.mk)
 
 # Inherit from the Lineage configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_dm1q
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 2160
+
+PRODUCT_NAME := voltage_dm1q
 PRODUCT_DEVICE := dm1q
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-S911B
