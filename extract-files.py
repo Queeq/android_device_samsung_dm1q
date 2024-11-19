25f7ddccc2b2b7dba91e25c4aacb100a09574945
Mezaque <mezaquegit@gmail.com>
2024-11-19 09:18:50 -0300

    dm1q: Adapt extractor utils to python
    Change-Id: Ia78ccc795bffb803f56cc4a3c948b76296f97a70

---
+++ extract-files.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/samsung/sm8550-common',
+]
+
+module = ExtractUtilsModule(
+    'dm1q',
+    'samsung',
+    namespace_imports=namespace_imports,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sm8550-common', module.vendor)
+    utils.run()
